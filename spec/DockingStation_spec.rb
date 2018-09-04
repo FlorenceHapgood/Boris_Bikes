e4d2390c6722485d3d682420a5ae2011eaa152e6
Florence Hapgood <florencehapgood@Florences-MacBook-Pro.local>
2018-09-04 09:20:48 -0700

    rspec

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,9 +1,16 @@
 require "./lib/DockingStation.rb"
 
+# describe DockingStation do
+#   it { is_expected.to respond_to :release_bike }
+#     it 'releases working bikes' do
+#       bike = subject.release_bike
+#       expect(bike).to be_working
+#     end
+#  end
+
+
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
-    it 'releases working bikes' do
-      bike = subject.release_bike
-      expect(bike).to be_working
-    end
- end
+  it "responds to release bike" do
+    expect(DockingStation.new).to respond_to :release_bike)
+  end
+end
